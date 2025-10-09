7811af35d5442241ded9f1ce1b5c968b22ff0865
drinduka383 <drin.duka@gmail.com>
2025-10-09 13:53:57 +0200

    2025-10-09

---
+++ ConsoleApplication20251009/20251009.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+	// Detyra 1
+
+	/*
+	int a;
+	a = 5;
+	string b = "Hello World!";
+
+	cout << b << endl;
+
+	cout << (5. / 2 + 5.5) << endl << endl;
+
+	cout << ((12.6 / 2) + 4) << endl << endl;
+	*/
+
+	// Detyra 2
+	/*
+	double a,b;
+
+	cout << "Shkruani vleren e variables se pare: ";
+	cin >> a;
+
+	cout << endl << "Shkruani vleren e variables se dyte: ";
+	cin >> b;
+
+	cout << endl << "Shuma e dy variablave eshte: " << a << " + " << b << " = " << a + b << endl;
+	*/
+
+	// Detyra 3
+
+	/*
+	float n1, n2, n3;
+
+	cout << "Shkruani vleren e notes se pare: ";
+	cin >> n1;
+	cout << "Shkruani vleren e notes se dyte: ";
+	cin >> n2;
+	cout << "Shkruani vleren e notes se trete: ";
+	cin >> n3;
+
+	cout << endl << "Nota mesatare e tre studenteve eshte: " << setprecision(2) << (n1 + n2 + n3)/3. << endl;
+	*/
+
+	// Detyra 4
+
+	/*
+	cout << "\"" << endl;
+	*/
+
+}
